144bd590ee30a82049bb20e983c131492acf812b
Jack <jacktchuente@gmail.com>
2022-12-04 09:05:44 -0500

    upd runme

--- docker/runme.sh
+++ docker/runme.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
 python manage.py create_admin
-gunicorn {{project_name}}.wsgi:application -p 8000 & nginx -g "daemon off;"
+sh run_celery.bat & sh run_celery_beat.bat & gunicorn {{project_name}}.wsgi:application -p 8000 & nginx -g "daemon off;"
