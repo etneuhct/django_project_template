f49a16452dbb598cb21b57d4c9f56f9a3dea2970
jack <>
2023-12-12 16:25:52 -0500

    add celery base

--- docker/runme.sh
+++ docker/runme.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
 python manage.py create_admin
-gunicorn {{project_name}}.wsgi:application -p 8000 & nginx -g "daemon off;"
+gunicorn api_core.wsgi:application -p 8000 & nginx -g "daemon off;"
